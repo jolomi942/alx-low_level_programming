d7093d45b1e8be298977937d01c6df7e420ddc36
Esther Jolomi <jolomi942@gmail.com>
2023-02-17 10:53:12 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,23 +7,13 @@
 
 int main(void)
 {
-	int tens;
-	int ones;
+	char alphabets = 'a';
 
-	for (tens = 0; tens <= 9; tens++)
+	while (alphabets <= 'z')
 	{
-		for (ones = tens + 1; ones  <= 9; ones++)
-		{
-			putchar(tens + '0');
-			putchar(ones + '0');
-
-			if (tens < 8)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+		putchar(alphabets);
+		alphabets++;
 	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
